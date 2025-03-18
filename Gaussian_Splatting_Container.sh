48ebf836d8b26525b936fce0344aaf1e2d9c2515
Nicholas Piperni <36056036+npiperni@users.noreply.github.com>
2025-03-18 01:41:05 -0400

    Update Gaussian_Splatting_Container.sh

--- Gaussian_Splatting_Container.sh
+++ Gaussian_Splatting_Container.sh
@@ -11,6 +11,15 @@
 echo "$0 : Starting Gaussian Splatting job on Speed..."
 date
 
+REPO_PATH="/speed-scratch/$USER/gaussian_splatting/"
+if [ ! -d $REPO_PATH]; then
+    mkdir -p $REPO_PATH
+    cd $REPO_PATH
+    git clone https://github.com/graphdeco-inria/gaussian-splatting --recursive
+fi
+
+cd /speed-scratch/$USER/
+
 # Load necessary modules
 SINGULARITY=/encs/pkg/singularity-3.10.4/root/bin/singularity
 
@@ -25,7 +34,7 @@ CONTAINER_PATH="/speed-scratch/$USER/3d_gaussian_splatting.sif"
 # Check if the container already exists, if not, pull and convert it
 if [ ! -f "$CONTAINER_PATH" ]; then
     echo "Pulling container from Docker Hub..."
-    $SINGULARITY pull --disable-cache "$CONTAINER_PATH" docker://gaetanlandreau/3d-gaussian-splatting
+    $SINGULARITY pull --disable-cache "$CONTAINER_PATH" docker://j3soon/gaussian_splatting
 else
     echo "Container already exists at $CONTAINER_PATH. Skipping download."
 fi
