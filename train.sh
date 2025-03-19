75a0b662459e318a538d1e1b95413a2cf11fb968
Nicholas Piperni <36056036+npiperni@users.noreply.github.com>
2025-03-19 01:14:02 -0400

    Variable dataset

--- train.sh
+++ train.sh
@@ -9,7 +9,15 @@
 #SBATCH --export=ALL                         ## Export all environment variables
 
 
-echo "$0 : Starting Gaussian Splatting job on Speed..."
+# Check if an argument was provided
+if [ -z "$1" ]; then
+    echo "Usage: sbatch train.sh <dataset_folder>"
+    exit 1
+fi
+
+DATASET_FOLDER=$1  # Get the first argument passed to sbatch
+
+echo "$0 : Starting Gaussian Splatting job on Speed with dataset $DATASET_FOLDER..."
 date
 
 SINGULARITY=/encs/pkg/singularity-3.10.4/root/bin/singularity
@@ -18,7 +26,7 @@ $SINGULARITY run --nv \
 --bind /speed-scratch/n_piper/dataset:/mnt/data \
 --bind /speed-scratch/n_piper/output:/workspace/gaussian-splatting/output \
 /speed-scratch/n_piper/3d_gaussian_splatting.sif \
-bash -c "echo 'Doing some setup...' && cd /workspace/gaussian-splatting/ && conda init bash && source ~/.bashrc && conda activate gaussian_splatting && python train.py -s /mnt/data/db/playroom/"
+bash -c "echo 'Doing some setup...' && cd /workspace/gaussian-splatting/ && conda init bash && source ~/.bashrc && conda activate gaussian_splatting && python train.py -s /mnt/data/$DATASET_FOLDER/"
 
 
 
