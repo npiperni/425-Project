b9fb02339b4237b816e3eaf1354d173248224fc9
Nicholas Piperni <36056036+npiperni@users.noreply.github.com>
2025-03-27 16:33:21 -0400

    update train to set output folder name

--- train.sh
+++ train.sh
@@ -9,13 +9,26 @@
 #SBATCH --export=ALL                         ## Export all environment variables
 
 
-# Check if an argument was provided
-if [ -z "$1" ]; then
-    echo "Usage: sbatch train.sh <dataset_folder>"
+# Check if the required arguments are provided
+if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "Usage: sbatch train.sh <dataset_folder> <output_folder>"
     exit 1
 fi
 
-DATASET_FOLDER=$1  # Get the first argument passed to sbatch
+DATASET_FOLDER=$1  # First argument: dataset folder
+OUTPUT_FOLDER=$2   # Second argument: output folder name
+
+# Define base output directory
+BASE_OUTPUT_DIR="/speed-scratch/$USER/output"
+
+# Full path for the output directory
+FULL_OUTPUT_DIR="$BASE_OUTPUT_DIR/$OUTPUT_FOLDER"
+
+# Create the output folder if it doesn't exist
+if [ ! -d "$FULL_OUTPUT_DIR" ]; then
+    mkdir -p "$FULL_OUTPUT_DIR"
+    echo "Created output directory: $FULL_OUTPUT_DIR"
+fi
 
 echo "$0 : Starting Gaussian Splatting job on Speed with dataset $DATASET_FOLDER..."
 date
@@ -23,11 +36,10 @@ date
 SINGULARITY=/encs/pkg/singularity-3.10.4/root/bin/singularity
 
 $SINGULARITY run --nv \
---bind $DATASET_FOLDER:/mnt/data \
---bind /speed-scratch/$USER/output:/workspace/gaussian-splatting/output \
+--bind "$DATASET_FOLDER:/mnt/data" \
+--bind "$FULL_OUTPUT_DIR:/workspace/gaussian-splatting/output" \
 /speed-scratch/$USER/3d_gaussian_splatting.sif \
 bash -c "echo 'Doing some setup...' && cd /workspace/gaussian-splatting/ && conda init bash && source ~/.bashrc && conda activate gaussian_splatting && python train.py -s /mnt/data/"
-
 
 
 
